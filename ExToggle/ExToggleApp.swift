1a6c3caff43af285b512523610534b7ac7176e68
jake <palatable7@naver.com>
2022-08-08 21:54:24 +0900

    Initial Commit

---
+++ ExToggle/ExToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExToggleApp.swift
+//  ExToggle
+//
+//  Created by 김종권 on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct ExToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
